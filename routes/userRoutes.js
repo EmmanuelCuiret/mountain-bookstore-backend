6021daae8b4deb09a1c9dbfba391ad4516a68053
EmmanuelCuiret <e.cuiret@gmail.com>
2025-03-09 23:56:33 +0100

    change route login post

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 // 📌 Logue un utilisateur
-router.get('/login', userController.getUserByLogin);
+router.post('/login', userController.getUserByLogin);
 
 // 📌 Créé un utilisateur (non utilisé)
 router.post('/', userController.createUser);
